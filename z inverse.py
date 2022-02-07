df31bc1a0dac8b9a556f7c17dfc045b36ad583ce
unknown <irfanahamed.n786@gmail.com>
2022-02-07 14:57:34 +0530

    Create z inverse.py

---
+++ z inverse.py
@@ -0,0 +1,15 @@
+n=int(input("rows:"))
+for i in range(0, n):
+    print(" "*i, end="")
+    print(" "*((n-i)*2-1), end="")
+    print("*"*i)
+
+print("*"*((2*n)-1))
+
+
+
+
+for i in range(n-1,0,-1):
+    print("*"*(i), end="")
+    print(" "*(n-i)*2, end="")
+    print(""*i)
